22fa6a7267c21bd65fb9f71bf0b35a0c530b3d91
Emmyliano <akemuemmy@gmail.com>
2024-09-08 22:55:41 +0100

    removed unnecessary files

--- Src/Header_files/binary.h
+++
@@ -1,8 +0,0 @@
-#ifndef BINARY_H_
-#define BINARY_H_
-
-//Function prototypes
-void B2D ( );
-void D2B ( );
-
-#endif
--- Src/Header_files/standard.h
+++
@@ -1,6 +0,0 @@
-#ifndef STANDARD_H_
-#define STANDARD_H_
-
-//Function prototypes
-int main ( );
-#endif
